ab16c1e2e5dc752a8cc8ddad8b851679076fafbd
Majid Tooranisama <mtooranisama5070@conestogac.on.ca>
2021-03-20 21:50:36 -0400

    DAL business object

--- js/dropshippingDAL.js
+++ js/dropshippingDAL.js
@@ -50,6 +50,52 @@ var Register = {
 
 };
 
+var BusinessInfo = {
+
+    businessInsert: function (options) {
+        function successInsertTransaction() {
+            console.info("Success: Insert Transaction successful");
+        }
+
+        function successInsert() {
+            console.info("Success: Insert successful");
+            alert("Business registration has been successful");
+
+            window.location.replace("page-user-login.html");
+        }
+
+        function txFunction(tx) {
+            var sql = "";
+            console.info("Start inserting into table business info");
+
+            sql = "INSERT INTO manufacturers(companyName, companyUrl, bnNumber, cFirstName, cLastName, contactTitle, cCountryCode, cPhoneNumber, cFaxNumber, contactEmail, contactCity, contactCountry, cAddressDetails, cPostalCode) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+            tx.executeSql(sql, options, successInsert, errorHandler);
+
+            console.info("Business data added");
+        }
+
+        db.transaction(txFunction, errorHandler, successInsertTransaction);
+    },
+
+    selectAll: function (callback) {
+        var options = [];
+
+        function successTransaction() {
+            console.info("Success: Select all Transaction successful");
+        }
+
+        function txFunction(tx) {
+            console.info("Selecting all records...");
+
+            var sql = "SELECT * FROM manufacturers;";
+            tx.executeSql(sql, options, callback, errorHandler);
+        }
+
+        db.transaction(txFunction, errorHandler, successTransaction);
+    },
+
+};
+
 var SignIn = {
 
     selectedUser: function (callback, options) {
@@ -82,8 +128,8 @@ var SignIn = {
             console.info("Success: Select all Transaction successful");
             alert("Registered user!");
             window.alert("You logged in successfully");
-             
-            window.location.replace("index.html");              
+
+            window.location.replace("index.html");
         }
 
         function txFunction(tx) {
